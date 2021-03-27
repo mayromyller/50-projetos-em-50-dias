e7ffc078df952a3769cee3735517de228e13d85d
mayro myller <deehmyller11@gmail.com>
2021-03-27 11:54:00 -0300

    Add color on background inputs add event to focus

--- coloors-project/src/app.js
+++ coloors-project/src/app.js
@@ -1,5 +1,4 @@
 const colorDivs = document.querySelectorAll(".color");
-const titleHex = document.querySelectorAll(".title");
 
 function generateHex() {
   const hexColor = chroma.random();
@@ -16,6 +15,15 @@ function randomColors() {
     hexText.innerHTML = randomColor;
 
     checkTextContrast(randomColor, hexText);
+
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorize(color, hue, brightness, saturation);
   });
 }
 
@@ -27,6 +35,54 @@ function checkTextContrast(color, text) {
     text.style.color = "white";
     text.style.filter = "drop-shadow(0.4rem 0.4rem 1rem rgba(0, 0, 0, 0.4))";
   }
+}
+
+function colorize(color, hue, brightness, saturation) {
+  const noSaturation = color.set("hsl.s", 0);
+  const fullSaturation = color.set("hsl.s", 1);
+  const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation]);
+
+  const midBrightness = color.set("hsl.l", 0.5);
+  const scaleBrightness = chroma.scale(["black", midBrightness, "white"]);
+
+  saturation.style.backgroundImage = `linear-gradient(
+    to right, 
+    ${scaleSaturation(0)}, 
+    ${scaleSaturation(1)}
+  )`;
+
+  brightness.style.backgroundImage = `linear-gradient(
+    to right, 
+    ${scaleBrightness(0)},
+    ${scaleBrightness(0.5)}, 
+    ${scaleBrightness(1)}
+  )`;
+
+  hue.style.backgroundImage = `linear-gradient(
+    to right, 
+    rgb(204,75,75),
+    rgb(204,204,75),
+    rgb(75,204,75),
+    rgb(75,204,204),
+    rgb(75,75,204),
+    rgb(204,75,204),
+    rgb(204,75,74)
+  )`;
+
+  boxshadowOnFocus(saturation, color);
+  boxshadowOnFocus(brightness, color);
+  boxshadowOnFocus(hue, color);
+}
+
+function boxshadowOnFocus(item, color) {
+  item.addEventListener(
+    "focusin",
+    (element) => (element.target.style.boxShadow = `0 0 0 3px ${color}`)
+  );
+  item.addEventListener(
+    "focusout",
+    (element) => (element.target.style.boxShadow = "none")
+  );
 }
 
 randomColors();
