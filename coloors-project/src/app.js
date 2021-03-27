48fff95886f4e52e513b6a5bb81e5258942743b3
mayro myller <deehmyller11@gmail.com>
2021-03-27 08:53:01 -0300

    Generate colors with chorma-js library

--- coloors-project/src/app.js
+++ coloors-project/src/app.js
@@ -1,1 +1,19 @@
-console.log('Start Project')
+const colorDivs = document.querySelectorAll(".color");
+
+function generateHex() {
+  const hexColor = chroma.random();
+
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    div.style.backgroundColor = randomColor;
+    hexText.innerHTML = randomColor;
+  });
+}
+
+randomColors();
