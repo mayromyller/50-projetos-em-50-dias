1123db162b2e558b4adbba224fbd9106a2a1a024
mayro myller <deehmyller11@gmail.com>
2021-04-03 17:08:32 -0300

    feat: add toogle active function

--- hidden-search/src/app.js
+++ hidden-search/src/app.js
@@ -1,1 +1,8 @@
-console.log('Start Project')
+const search = document.querySelector(".search");
+const searchAction = document.querySelector(".search-action");
+const input = document.querySelector(".input");
+
+searchAction.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
