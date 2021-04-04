e2fe4eda078ef869bd77ebace62dd676267c1598
mayro myller <deehmyller11@gmail.com>
2021-04-04 07:12:27 -0300

    feat: add function blurrinig on load

--- blurry-loading/src/app.js
+++ blurry-loading/src/app.js
@@ -1,1 +1,22 @@
-console.log('Start Project')
+const hero = document.querySelector(".hero");
+const loading = document.querySelector(".loading");
+
+let load = 0;
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+const int = setInterval(blur, 30);
+
+function blur() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  loading.innerText = `${load}%`;
+  loading.style.opacity = scale(load, 0, 100, 1, 0);
+  hero.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
