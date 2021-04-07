b303ba5f9582cb0e25df305d6eeb47614bdf8dc2
mayro myller <deehmyller11@gmail.com>
2021-04-07 17:19:31 -0300

    feat: add mouse events

--- 08-landing-page/src/app.js
+++ 08-landing-page/src/app.js
@@ -1,1 +1,19 @@
-console.log('Start Project')
+const left = document.querySelector(".left");
+
+const right = document.querySelector(".right");
+
+const container = document.querySelector(".container");
+
+left.addEventListener("mouseenter", () =>
+  container.classList.add("hover-left")
+);
+left.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-left")
+);
+
+right.addEventListener("mouseenter", () =>
+  container.classList.add("hover-right")
+);
+right.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-right")
+);
