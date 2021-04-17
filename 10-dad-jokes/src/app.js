357c8cb7797434687c386945d128502e3fd3653e
Mayro <deehmyller11@gmail.com>
2021-04-17 17:29:10 -0300

    feat: fetching the api of incahazdadjoke

--- 10-dad-jokes/src/app.js
+++ 10-dad-jokes/src/app.js
@@ -1,1 +1,21 @@
-console.log('Start Project')
+const url = "https://icanhazdadjoke.com/";
+
+const options = {
+  headers: {
+    Accept: "application/json",
+  },
+};
+
+const jokeElement = document.querySelector(".joke");
+const buttonGetJoke = document.querySelector(".get-joke");
+
+async function getJoke() {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  jokeElement.innerHTML = data.joke;
+}
+
+buttonGetJoke.addEventListener("click", getJoke);
+
+getJoke();
