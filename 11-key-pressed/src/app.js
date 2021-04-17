c905aed9e86d3350c3ae8cb4040498d687a19861
Mayro <deehmyller11@gmail.com>
2021-04-17 17:57:58 -0300

    feat: get the key pressed on windown event

--- 11-key-pressed/src/app.js
+++ 11-key-pressed/src/app.js
@@ -1,1 +1,24 @@
-console.log('Start Project')
+const container = document.querySelector(".container");
+
+window.addEventListener("keydown", (pressed) => {
+  container.innerHTML = `
+    <div class="key-container">
+      <h3 class="title">event.key</h3>
+      <div class="box">
+      ${pressed.key === " " ? "Space" : pressed.key}
+      </div>
+    </div>
+    <div class="key-container">
+      <h3 class="title">event.keyCode</h3>
+      <div class="box">
+      ${pressed.keyCode}
+      </div>
+    </div>
+    <div class="key-container">
+      <h3 class="title">event.code</h3>
+      <div class="box">
+      ${pressed.code}
+      </div>
+    </div>
+  `;
+});
